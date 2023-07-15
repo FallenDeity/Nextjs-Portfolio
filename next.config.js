b462157227afd51711b761c10fd67fa5b436d4e9
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-07-15 21:10:57 +0530

    feat: Add workflow for github pages

--- next.config.js
+++ next.config.js
@@ -1,13 +1,30 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "/";
+let images = {
+	remotePatterns: [
+		{
+			protocol: "https",
+			hostname: "**",
+		},
+	],
+};
+if (isGithubActions) {
+	const repoName = process.env.GITHUB_REPOSITORY.replace(/.*\//, "");
+	assetPrefix = `/${repoName}/`;
+	basePath = `/${repoName}`;
+	images = {
+		loader: "imgix",
+		path: "https://personal-2066.imgix.net/",
+	}
+}
+
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "**",
-			},
-		],
-	},
+	assetPrefix: assetPrefix,
+	basePath: basePath,
+	images: images,
 	reactStrictMode: true,
 	distDir: "dist",
 };
