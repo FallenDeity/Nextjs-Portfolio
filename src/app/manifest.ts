c0ca5d24734dc5ae4fc636cec785f75000553e98
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-09-20 02:55:24 +0530

    feat: optimise website

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,9 +12,24 @@ export default function manifest(): MetadataRoute.Manifest {
 		theme_color: "#5c09b5",
 		icons: [
 			{
-				src: "/favicon.ico",
-				sizes: "any",
-				type: "image/x-icon",
+				src: "/icons/icon-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-256x256.png",
+				sizes: "256x256",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-384x384.png",
+				sizes: "384x384",
+				type: "image/png",
+			},
+			{
+				src: "/icons/icon-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
 			},
 		],
 	};
