d7c5e1f5c5377fab562593102690e054999de8b1
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-09-20 02:35:24 +0530

    feat: optimise website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
 import "@/styles/globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
 import React from "react";
 
-export const metadata: Metadata = {
-	title: "Portfolio",
-	description: "Triyan's Portfolio",
-};
+import { meta } from "@/lib/utils";
+
+export const metadata: Metadata = meta;
 
 export default function RootLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
 	return (
 		<html lang="en">
-			<body className="dark scrollbar-thin scrollbar-track-black-100 scrollbar-thumb-gray-700">{children}</body>
+			<body className="dark scrollbar-thin scrollbar-track-black-100 scrollbar-thumb-gray-700">
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
